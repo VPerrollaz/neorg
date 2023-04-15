7597c24b90a4f2ead6621728e0ab162c9bc9480a
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2023-04-15 13:04:45 +0200

    Transform get_node_text
    from vim.treesitter.query to vim.treesitter

--- lua/neorg/modules/core/norg/qol/toc/module.lua
+++ lua/neorg/modules/core/norg/qol/toc/module.lua
@@ -199,7 +199,7 @@ module.public = {
 
           local line, _, _ = node:start()
           local heading_text_node = ts.get_first_node("paragraph_segment", 0, node)
-          local heading_text = vim.split(vim.treesitter.query.get_node_text(heading_text_node, 0), "\n")
+          local heading_text = vim.split(vim.treesitter.get_node_text(heading_text_node, 0), "\n")
 
           local prefix = string.rep(display_as_links and "-" or "*", heading_level)
             .. (display_as_links and "> " or " ")
